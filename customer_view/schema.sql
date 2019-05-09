0f42f778a745fa4de8e7ef97fbe3e0daab88d471
JP <parker.tech9@gmail.com>
2019-05-09 17:47:23 -0400

    inital files

---
+++ customer_view/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS bamazon;
+
+use bamazon;
+
+CREATE TABLE IF NOT EXISTS products(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(30),
+    department_name VARCHAR(30),
+    price INT,
+    stock_quantity INT
+);
